da1643acb61ec833245245fd56bf9d44444e65a3
Dmitry Zakharchenko <wilsonept@gmail.com>
2023-01-12 17:56:52 +0300

    Исправления скриптов.
    mouse-sensitivity - теперь не привязана к id.
    vpnconnect - изменен сертификат для подключения.

--- .config/i3/mousesensitivity.sh
+++ .config/i3/mousesensitivity.sh
@@ -12,5 +12,9 @@
 #    xinput list-props <device_id>
 
 # xinput --set-prop <device_id> <param_id> <X 0 0>  <0 Y 0> <0 0 multipiler>
+# <device_id> can be the name of the device. Sometimes there are multiple 
+# devcies matching one name. To ensure the correct one is selected, please 
+# use the device ID, or prefix the device name with 'pointer:' or 'keyboard:'
+# as appropriate.
 # P.S. default values for multipiler, X and Y is "1"
-xinput --set-prop 11 158 0.35 0 0 0 0.35 0 0 0 1
+xinput set-prop "pointer:COMPANY  USB Device " 158 0.35 0 0 0 0.35 0 0 0 1
